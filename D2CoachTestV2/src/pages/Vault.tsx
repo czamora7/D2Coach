0f557b163f57ae40c4ad8007916695935f2193e0
czamora7 <czamora4@ewu.edu>
2024-02-26 23:18:29 -0800

    attempting to center a div

--- D2CoachTestV2/src/pages/Vault.tsx
+++ D2CoachTestV2/src/pages/Vault.tsx
@@ -16,42 +16,47 @@ const t1data = [["","","",""],
  //console.log("Debugger check... authCode: " + localStorage.getItem("authCode") + " Token: " + localStorage.getItem("userToken"));
   return <Fragment>
 
-      <div>
-        <div className="weapons">
+      <div className="weapons">
+        <div className="kinetic">
           <h1>Kinetic Weapons</h1>
+          <br></br>
           <table className="weaponsTable">
             <VaultDisplay rows={t1data} />
           </table>
         </div>
 
-        <div className="weapons">
+        <div className="energy">
           <h1>Energy Weapons</h1>
+          <br></br>
           <table className="weaponsTable">
             <VaultDisplay rows={t1data} />
           </table>
         </div>
 
-        <div className="weapons">
+        <div className="heavy">
           <h1>Heavy Weapons</h1>
+          <br></br>
           <table className="weaponsTable">
-            <VaultDisplay rows={t1data} />
-          </table>
-        </div>
-
-        <br></br>
-
-        <div>
-          <h1>Armor</h1>
-          <table className="armorTable">
             <VaultDisplay rows={t1data} />
           </table>
         </div>
       </div>
 
+        <br></br>
+
+        <div className="armor">
+          <h1>Armor</h1>
+          <br></br>
+          <table className="armorTable">
+            <VaultDisplay rows={t1data} />
+          </table>
+        </div>
+
       <br></br>
 
-      <div>
+      <div className="armor">
         <h1>Miscellaneous</h1>
+        <br></br>
         <table className="armorTable">
           <VaultDisplay rows={t1data} />
         </table>
