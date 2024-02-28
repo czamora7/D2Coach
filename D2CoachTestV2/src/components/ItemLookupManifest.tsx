006c63d7d235467436b8728feb3cefe044ad4951
Brennen Kane-Christensen <bkanechristensen@eagles.ewu.edu>
2024-02-27 21:29:51 -0800

    Implementing Start of Character Data Pulling

---
+++ D2CoachTestV2/src/components/ItemLookupManifest.tsx
@@ -0,0 +1,7 @@
+import { useState, useEffect } from "react";
+
+async function dataLookup(idNum: number, table: string) {
+    let convertedId = idNum >> 32;
+
+
+}
--- D2CoachTestV2/src/pages/Debugger.tsx
+++ D2CoachTestV2/src/pages/Debugger.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 
 // import getManifest from './apiRequests/getDestinyManifest';
-// import getCurrentUser from './apiRequests/getUserProfile';
-import convertToSignedInt from './components/UnsignedToSigned';
+import getCurrentUser from '../apiRequests/getUserProfile';
+import getAccInfo from '../apiRequests/getNeededAccData';
+
 
 const Debugger: React.FC = () => {
 
  // getManifest();
- // getCurrentUser();
+ getAccInfo();
 
  // console.log("Debugger check... authCode: " + localStorage.getItem("authCode") + " Token: " + localStorage.getItem("userToken"));
-var x = 3381450498
-var y = convertToSignedInt(3381450498);
- console.log("The result of " + x + " converted is: " + y);
+ 
   return <div>Welcome to the Debugger/Console.log() Page.
     <p></p>
   </div>;
