6cab68d52bcac88d7697554e2d23424c3425006f
czamora7 <czamora4@ewu.edu>
2024-03-13 10:30:59 -0700

    added fallback image property to thumbnail component

--- D2CoachTestV2/src/components/LoadoutDisplay.tsx
+++ D2CoachTestV2/src/components/LoadoutDisplay.tsx
@@ -1,5 +1,6 @@
 import Thumbnail from "./Thumbnail"
 import '../styles/LoadoutBuilder.css';
+import imagePath from "../assets/Images/alt.png";
 
 interface DisplayProps {
     subclass: string;
@@ -17,9 +18,9 @@ function LoadoutDisplay({subclass,exoticArmor,exoticWeapon}:DisplayProps) {
                 <th>Exotic Weapon</th>
             </tr>
             <tr>
-                <td><Thumbnail src={subclass}></Thumbnail></td>
-                <td><Thumbnail src={exoticArmor}></Thumbnail></td>
-                <td><Thumbnail src={exoticWeapon}></Thumbnail></td>
+                <td><Thumbnail src={subclass} fallback={imagePath}></Thumbnail></td>
+                <td><Thumbnail src={exoticArmor} fallback={imagePath}></Thumbnail></td>
+                <td><Thumbnail src={exoticWeapon} fallback={imagePath}></Thumbnail></td>
             </tr>
             </tbody>
         </table>
--- D2CoachTestV2/src/components/Thumbnail.tsx
+++ D2CoachTestV2/src/components/Thumbnail.tsx
@@ -1,14 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment,useState } from 'react';
 import '../styles/Thumbnail.css'
 
 interface ThumbnailProps {
   src:string;
+  fallback:string;
 }
 
-function Thumbnail({src}:ThumbnailProps) {
+function Thumbnail({src,fallback}:ThumbnailProps) {
+    const [imgSrc,setImgSrc] = useState<string>(src);
+    const onError = () => setImgSrc(fallback);
+
     return (
       <Fragment>
-          <img src={src} width = "60" height="60" />
+          <img src={imgSrc} width ="60" height="60" onError={onError} />
       </Fragment>
       );
 
